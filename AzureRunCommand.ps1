413d85df3933ca6fd7c5daa4737dab6d2ea61b10
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2019-01-10 00:45:20 +0530

    Delete AzureRunCommand.ps1

--- AzureRunCommand.ps1
+++
@@ -1,5 +0,0 @@
-﻿Login-AzureRmAccount
-Select-AzureRmSubscription -SubscriptionId "cef04a14-22ed-4db1-9785-ccc5bc19494c"
-$ResourceGroupName="RG_OneShareDevTeam"
-$VMName="OneShareInfraVM"
-Invoke-AzureRmVMRunCommand -ResourceGroupName $ResourceGroupName -Name $VMName -CommandId 'RunPowerShellScript' -ScriptPath 'C:\Users\gdusane\Documents\powershellscript.ps1'
