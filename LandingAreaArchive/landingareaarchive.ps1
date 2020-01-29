45b1300913497317ba46b6b43121057bd93fa2e7
Ghanshyam Dusane <37286886+ghanshyamdusane@users.noreply.github.com>
2020-01-29 20:45:31 +0530

    Update landingareaarchive.ps1

--- LandingAreaArchive/landingareaarchive.ps1
+++ LandingAreaArchive/landingareaarchive.ps1
@@ -4,22 +4,26 @@ $IMPORT=Import-Csv -Path $WorkDir\StorageAccountDeatils.csv
 $FileName=("files-$(Get-Date)" -split " ")[0]
 $Days = 7
 $CBCKDays = 2
+$DTE = (Get-Date).Date
+$DTE = $DTE.AddDays(-$Days)
+Write-Host $DTE
+$DTECBCK = (Get-Date).Date
+$DTECBCK = $DTECBCK.AddDays(-$CBCKDays)
+Write-Host $DTECBCK
 $sgusername = "azure_<>@azure.com"
-$sgpassword = "<pw>"
+$sgpassword = "<>"
 $secureStringPwd = $sgpassword | ConvertTo-SecureString -AsPlainText -Force 
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $sgusername, $secureStringPwd
 $sub="Landing Area Archival Notification $(Get-Date)"
 $body="Hello All, 
 This is notification send by the Automation.
-Kindly find the attached sheet which contains files moved  on $(Get-Date)
+Kindly find the attached sheet which contains files moved on $(Get-Date)
 Regards,
 IIoT Automation"
 
 $From = "LandingAreaArchiveAutomation@domain.com"
-#$To1 = ""
-#$To2 = ""
-$To1 = "dusanegs@gmail.com"
-$Cc = ""
+$To1 = "Ghanshyam.Dusane@.com"
+$Cc = "ghanshyam.dusane@.com"
 
 foreach ( $data in $IMPORT ) {
 
@@ -36,7 +40,7 @@ $finalCBCKname=$($FileCBCKName -replace "/" , "-")  + ".csv"
 $StorageAccountContext = New-AzureStorageContext -StorageAccountName $STGACCNAME -StorageAccountKey $STGACCNAMEKEY -ErrorAction Stop
 
 $COPYDATAs = Get-AzureStorageBlob -Container $CONTAINERNAME -Context $StorageAccountContext | Where-Object { 
-$_.LastModified.DateTime -lt ((Get-Date).AddDays(-$Days)) -and  ( $_.Name -notmatch 'CB|CK' )
+$_.LastModified.DateTime -lt ( $DTE ) -and  ( $_.Name -notmatch 'CB|CK' )
 } 
 $COPYDATAs | Export-Csv -Path $("$WorkDir"+ "\" + "$finalname")
 $FilePath=$("$WorkDir"+ "\" + "$finalname") 
@@ -44,7 +48,7 @@ $FilePath=$("$WorkDir"+ "\" + "$finalname")
 $COPYDATAs | Start-AzureStorageBlobCopy -DestContainer $DESTCONTAINERNAME -DestContext $StorageAccountContext -ErrorAction Stop -Force -Verbose
 
 $COPYCBCKDATAs = Get-AzureStorageBlob -Container $CONTAINERNAME -Context $StorageAccountContext | Where-Object { 
-$_.LastModified.DateTime -lt ((Get-Date).AddDays(-$CBCKDays)) -and  ( $_.Name -match 'CB|CK' ) 
+$_.LastModified.DateTime -lt ( $DTECBCK ) -and  ( $_.Name -match 'CB|CK' ) 
 }
 
 $COPYCBCKDATAs | Export-Csv -Path $("$WorkDir"+ "\" + "$finalCBCKname")
